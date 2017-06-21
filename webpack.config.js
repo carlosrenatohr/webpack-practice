a470997253989c990bd40bc483123f4b3d6c8dcf
Carlos Hernandez <honchkrow1995@gmail.com>
2017-06-21 00:03:05 -0600

    Bundle html and images

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ var extractTextWebPackPlugin = require('extract-text-webpack-plugin');
 var extractPlugin = new extractTextWebPackPlugin({
 	filename: 'styles.css'
 });
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/js/app.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: 'dist',
+		//publicPath: 'dist',
 		filename: 'bundle.js'
 	},
 	module: {
@@ -38,12 +39,31 @@ module.exports = {
 	          		presets: ['env']
 	        	}
 	        }
+		},
+		{
+			test: /\.html$/,
+			use: ['html-loader']
+		},
+		{
+			test: /\.(jpg|png)$/,
+			use: {
+				loader:'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'img/',
+					//publicPath: 'img/'
+				}
+			}
 		}
 		]
 	},
 	plugins: [
 		//new webpack.optimize.UglifyJsPlugin({/* .. */}),
 		extractPlugin,
+		new htmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
+		//-- Webpack 3
 		new webpack.optimize.ModuleConcatenationPlugin()
 	]
 }
